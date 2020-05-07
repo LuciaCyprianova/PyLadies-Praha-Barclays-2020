7b669e2358546d5f5b9b5fbe25fb9415f117b70d
Lucia <l.cyprianova@gmail.com>
2020-05-07 12:09:39 +0200

    kamen-papier-noznice_add_random
    set the variable tah_pocitace to be random out of three options with a use of "randrange"

--- 01_kamen-papier-noznice.py
+++ 01_kamen-papier-noznice.py
@@ -1,23 +1,36 @@
+from random import randrange
+
 print ("Hráme hru: kameň, papier, nožnice")
 
 tah_cloveka = input("Hráš kameň, papier alebo nožnice? (V prípade, že chceš hru ukončiť, napíš 'koniec') ")
 
 while tah_cloveka != "koniec":
-    tah_pocitace = "kameň"
-     
-    if (tah_cloveka == "kameň" and tah_pocitace == "nožnice") or (tah_cloveka == "papier" and tah_pocitace == "kameň") or (tah_cloveka == "nožnice" and tah_pocitace == "papier"):
-        print ("Počítač dal", tah_pocitace, "Vyhral si! Gratulujem")        
+    nahoda = randrange(0, 3)
+    if nahoda == 0:
+        tah_pocitace = "kameň"
+    elif nahoda == 1:
+        tah_pocitace = "papier"
+    else:
+        tah_pocitace = "nožnice"
 
-    elif (tah_cloveka == tah_pocitace):
-        print ("Počítač dal", tah_pocitace, "Remíza!")
-               
-    elif (tah_cloveka == "kameň" and tah_pocitace == "papier") or (tah_cloveka == "papier" and tah_pocitace == "nožnice") or (tah_cloveka == "nožnice" and tah_pocitace == "kameň"):
-        print ("Počítač dal", tah_pocitace, "Prehral si :(")
-             
+    if (tah_cloveka == "kameň") or (tah_cloveka == "papier") or (tah_cloveka == "nožnice"):
+
+        if (tah_cloveka == "kameň" and tah_pocitace == "nožnice") or (tah_cloveka == "papier" and tah_pocitace == "kameň") or (tah_cloveka == "nožnice" and tah_pocitace == "papier"):
+            print ("Počítač dal", tah_pocitace, "Vyhral si! Gratulujem")
+            print()
+           
+        elif (tah_cloveka == tah_pocitace):
+            print ("Počítač dal", tah_pocitace, "Remíza!")
+            print()
+
+        else:
+            print ("Počítač dal", tah_pocitace, "Prehral si :(")
+            print()
+
     else:
         print("Môžeš hrať iba kameň, papier alebo nožnice.")
-    
-    print()
+        print()
+
     tah_cloveka = input("Hráš kameň, papier alebo nožnice? (V prípade, že chceš hru ukončiť, napíš 'koniec') ")
-        
+
 print("Koniec hry.")
