21522d665caa0152d3154614ef65498440ef98ce
Lucia <l.cyprianova@gmail.com>
2020-05-07 12:31:18 +0200

    hod-kostkou_define function
    define function for hod_kostkou

--- 04_hod_kostkou.py
+++ 04_hod_kostkou.py
@@ -1,24 +1,25 @@
 from random import randrange
 
+def hod_kostkou(hrac):
+    "vráti počet pokusov, než hráčovi padla 6"
+    pocet_hodov = 0
+    while True:    
+        hod = randrange(1, 7)
+        print("Hráč", hrac, "hodil číslo", hod)
+        pocet_hodov = pocet_hodov + 1
+        if hod == 6:
+            print()
+            print("Hráč", hrac, "hodil", pocet_hodov, "krát")
+            print()
+            return pocet_hodov
+
 hody_max = 0
 vitaz = 0
 
-for hrac in range(1, 5):
-    
-    hod = 0
-    pocet_hodov = 0
- 
-    while hod != 6:
-        pocet_hodov = pocet_hodov + 1
-        hod = randrange(1, 7)
-        print("Hráč", hrac, "hodil číslo", hod)
-        
-    print()
-    print("Hráč", hrac, "hodil", pocet_hodov, "krát")
-    print()
-   
+for hrac in range(1, 5):        
+    pocet_hodov = hod_kostkou(hrac)    
     if pocet_hodov > hody_max:
         hody_max = pocet_hodov
         vitaz = hrac
 
-print("Vyhral hráč", vitaz)
+print("Vyhral hráč číslo", vitaz)
