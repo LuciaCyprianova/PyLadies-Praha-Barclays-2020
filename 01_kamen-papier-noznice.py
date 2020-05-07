f57e96347f2e95661203081498a430036f75c773
Lucia <l.cyprianova@gmail.com>
2020-05-07 11:51:59 +0200

    simplify the code
    use "and" and "or" to prevent repeating the same strings in the code

--- 01_kamen-papier-noznice.py
+++ 01_kamen-papier-noznice.py
@@ -1,31 +1,18 @@
 print ("Hráme hru: kameň, papier, nožnice")
 
-tah_cloveka = input("kameň, papier alebo nožnice? ")
+tah_cloveka = input("Hráš kameň, papier alebo nožnice? ")
 tah_pocitace = "kameň"
 
-if tah_cloveka == "kameň":
-    if tah_pocitace == "kameň":
-        print ("Počítač dal", tah_pocitace, "Remíza!")
-    elif tah_pocitace == "papier":
-        print ("Počítač dal", tah_pocitace, "Prehral si :(")
-    else:
+if (tah_cloveka == "kameň") or (tah_cloveka == "papier") or (tah_cloveka == "nožnice"):
+
+    if (tah_cloveka == "kameň" and tah_pocitace == "nožnice") or (tah_cloveka == "papier" and tah_pocitace == "kameň") or (tah_cloveka == "nožnice" and tah_pocitace == "papier"):
         print ("Počítač dal", tah_pocitace, "Vyhral si! Gratulujem")
 
-elif tah_cloveka == "papier":
-    if tah_pocitace == "papier":
+    elif (tah_cloveka == tah_pocitace):
         print ("Počítač dal", tah_pocitace, "Remíza!")
-    elif tah_pocitace == "nožnice":
-        print ("Počítač dal", tah_pocitace, "Prehral si :(")
-    else:
-        print ("Počítač dal", tah_pocitace, "Vyhral si! Gratulujem")   
 
-elif tah_cloveka == "nožnice":
-    if tah_pocitace == "nožnice":
-        print ("Počítač dal", tah_pocitace, "Remíza!")
-    elif tah_pocitace == "kameň":
-        print ("Počítač dal", tah_pocitace, "Prehral si :(")
     else:
-        print ("Počítač dal", tah_pocitace, "Vyhral si! Gratulujem")
+        print ("Počítač dal", tah_pocitace, "Prehral si :(")
 
 else:
     print("Môžeš hrať iba kameň, papier alebo nožnice.")
