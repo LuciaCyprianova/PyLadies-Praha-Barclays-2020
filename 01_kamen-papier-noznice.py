0b1310ce8a4b6554fa697921768b81cd1e53ad77
Lucia <l.cyprianova@gmail.com>
2020-05-07 12:06:32 +0200

    Update 01_kamen-papier-noznice.py with while cycle
    add while cycle to continue the game until the user gives the input "koniec" to exit the game

--- 01_kamen-papier-noznice.py
+++ 01_kamen-papier-noznice.py
@@ -1,18 +1,23 @@
 print ("Hráme hru: kameň, papier, nožnice")
 
-tah_cloveka = input("Hráš kameň, papier alebo nožnice? ")
-tah_pocitace = "kameň"
+tah_cloveka = input("Hráš kameň, papier alebo nožnice? (V prípade, že chceš hru ukončiť, napíš 'koniec') ")
 
-if (tah_cloveka == "kameň") or (tah_cloveka == "papier") or (tah_cloveka == "nožnice"):
-
+while tah_cloveka != "koniec":
+    tah_pocitace = "kameň"
+     
     if (tah_cloveka == "kameň" and tah_pocitace == "nožnice") or (tah_cloveka == "papier" and tah_pocitace == "kameň") or (tah_cloveka == "nožnice" and tah_pocitace == "papier"):
-        print ("Počítač dal", tah_pocitace, "Vyhral si! Gratulujem")
+        print ("Počítač dal", tah_pocitace, "Vyhral si! Gratulujem")        
 
     elif (tah_cloveka == tah_pocitace):
         print ("Počítač dal", tah_pocitace, "Remíza!")
-
-    else:
+               
+    elif (tah_cloveka == "kameň" and tah_pocitace == "papier") or (tah_cloveka == "papier" and tah_pocitace == "nožnice") or (tah_cloveka == "nožnice" and tah_pocitace == "kameň"):
         print ("Počítač dal", tah_pocitace, "Prehral si :(")
-
-else:
-    print("Môžeš hrať iba kameň, papier alebo nožnice.")
+             
+    else:
+        print("Môžeš hrať iba kameň, papier alebo nožnice.")
+    
+    print()
+    tah_cloveka = input("Hráš kameň, papier alebo nožnice? (V prípade, že chceš hru ukončiť, napíš 'koniec') ")
+        
+print("Koniec hry.")
