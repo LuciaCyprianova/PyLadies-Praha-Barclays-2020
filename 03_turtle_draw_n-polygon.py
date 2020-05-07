fe81fca7d3e0ea85f48d8c3cef2465682fca854d
Lucia <l.cyprianova@gmail.com>
2020-05-07 12:28:09 +0200

    turtle-draw-n-polygon_use own function
    define an own function to draw an n-polygon
    use for cycle to draw more polygons where "n" is being increased

--- 03_turtle_draw_n-polygon.py
+++ 03_turtle_draw_n-polygon.py
@@ -1,16 +1,16 @@
-from turtle import shape, speed, forward, left, right, penup, pendown, exitonclick
-from math import sqrt
-shape("turtle")
-speed(1)
+from turtle import forward, left, exitonclick, penup, pendown
 
-n = int(input("Zadaj počet vrcholov pravidelného n-uholníka: "))
+def nakresli_n_uholnik(pocet_stran):
+    uhel = 180 * (1 - 2 / pocet_stran)
+    delka_strany = 200 / pocet_stran
+    for n_uholnik in range(pocet_stran):
+        forward(delka_strany)
+        left(180 - uhel)
+    
+for n in range(5, 9):
+    nakresli_n_uholnik(n)
+    penup()
+    forward(80)
+    pendown()
 
-if n >= 3:
-    l = 330 / n
-    for i in range (n):
-        forward(l)
-        left(180 - (180 * (1 - (2/n))))
-    exitonclick()
-
-else:
-    print("Taký n-uholník neexistuje.")
+exitonclick()
